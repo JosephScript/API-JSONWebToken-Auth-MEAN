f2cefb8e38b85edcbd6992e5adb67694e3e1081a
Joseph Szczesniak <nukapunk@gmail.com>
2015-11-16 14:48:30 -0600

    Initial commit.

---
+++ routes/register.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var Users = require('../models/users');
+
+/* POST /api/register/ */
+router.post('/', function (req, res, next) {
+
+    req.check('username').isAlphanumeric(); // check to see if not empty
+
+    var errors = req.validationErrors();
+
+    if (errors){
+        res.status(400).send(errors);
+    } else {
+
+        Users.Create(req.body, function (err, user) {
+            if (err) {
+                res.status(400).send(err.message);
+            } else {
+                res.send(200);
+            }
+        });
+    }
+});
+
+module.exports = router;
